6b39a0f7d4cac6b858edf3903ec7e945e0137760
slontia <654867229@qq.com>
2022-05-28 21:55:39 +0800

    task: sleep

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if (argc < 2) {
+    fprintf(2, "sleep: missing operand\n");
+    exit(1);
+  }
+  for (int i = 1; i < argc; i++) {
+    sleep(atoi(argv[i]));
+  }
+  exit(0);
+}
